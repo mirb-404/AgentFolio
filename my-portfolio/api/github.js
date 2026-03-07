3f5c3ca626ee0e173d0527374fe125453a44d3f8
Bloodwingv2 <bhandariumesh81@gmail.com>
2026-03-07 15:35:43 +0100

    - Changed Header name

--- my-portfolio/api/github.js
+++ my-portfolio/api/github.js
@@ -64,7 +64,7 @@ export default async function handler(req, res) {
         };
 
         // Set caching headers for Vercel Edge Network
-        res.setHeader('Cache-Control', `s-maxage=${CACHE_DURATION}, stale-while-revalidate`);
+        res.setHeader('Cache-Control', `public, s-maxage=${CACHE_DURATION}, stale-while-revalidate=86400`);
         res.status(200).json(payload);
     } catch (error) {
         console.error("Serverless Function Error:", error);
